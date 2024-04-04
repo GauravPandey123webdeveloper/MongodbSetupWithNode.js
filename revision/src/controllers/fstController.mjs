4316894f5c96379aea9229b529259551ca185f2a
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-04-04 18:31:28 +0530

    post

--- revision/src/controllers/fstController.mjs
+++ revision/src/controllers/fstController.mjs
@@ -1,6 +1,8 @@
 import studentModel from "../models/studentModel.mjs";
 const findStudent=async (req,res)=>{
- return res.send('hello class');
+    const students= await studentModel.findOne({name:"Arihant Singh Rana"});
+    // const students= await studentModel.find({name:"Arihant Singh Rana"});
+ return res.send({status:'ok',message:students});
 }
 const createStudent=async (req,res)=>{
 const data= req.body;
--- revision/src/routes/routes.mjs
+++ revision/src/routes/routes.mjs
@@ -1,5 +1,6 @@
 import express from "express";
-import { findStudent } from "../controllers/fstController.mjs";
+import { findStudent,createStudent } from "../controllers/fstController.mjs";
 const router=express.Router();
-router.get('/',findStudent);
+router.get('/findStudent',findStudent);
+router.post('/addstudent',createStudent);
 export default router;
