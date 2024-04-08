205a8495494cd9925f894bc8a67858a5564d46de
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-04-08 12:28:53 +0530

    status code and error handling

--- SDMS/src/controllers/studentController.mjs
+++ SDMS/src/controllers/studentController.mjs
@@ -1,22 +1,31 @@
 import studentModel from "../models/studentModel.mjs";
-const findStudent= async (req,res)=>{
-    // const students= await studentModel.find();
-    const students= await studentModel.findOne();
-    res.send({status:true,message:students});
-}
-const createStudent= async (req,res)=>{
-   const data = req.body;
-   const createdStudent= await studentModel.create(data);
-//    const createdStudent= await studentModel.insertMany([data,data,data]);
-   return res.send({status:true,message:createdStudent})
-}
-const deleteStudent= async (req,res)=>{
-    // const data= await studentModel.deleteMany({name:'tiwari'});
-    const data= await studentModel.findByIdAndDelete({_id:"660f84b9acf3cfc26243838e"});
-    return res.send({status:true,message:data});
-}
-const updateStudent= async (req,res)=>{
-const newStudent= await studentModel.updateOne({"name":"raja"},{$set:{"name":"praja"}});
-return res.send({status:true,message:newStudent})
-}
-export {findStudent,createStudent,deleteStudent,updateStudent};
+const findStudent = async (req, res) => {
+  // const students= await studentModel.find();
+  const students = await studentModel.findOne();
+  res.status(200).send({ status: true, message: students });
+};
+const createStudent = async (req, res) => {
+  const data = req.body;
+  const createdStudent = await studentModel.create(data);
+  //    const createdStudent= await studentModel.insertMany([data,data,data]);
+  return res.status(201).send({ status: true, message: createdStudent });
+};
+const deleteStudent = async (req, res) => {
+  // const data= await studentModel.deleteMany({name:'tiwari'});
+  try {
+    const data = await studentModel.findByIdAndDelete({
+      _id: "660f8e593b4a8a4a0efc47d@",
+    });
+    return res.status(200).send({ status: true, message: data });
+  } catch (error) {
+    return res.status(500).send({status:'failed',message:error.message})
+  }
+};
+const updateStudent = async (req, res) => {
+  const newStudent = await studentModel.updateOne(
+    { name: "raja" },
+    { $set: { name: "praja" } }
+  );
+  return res.status(200).send({ status: true, message: newStudent });
+};
+export { findStudent, createStudent, deleteStudent, updateStudent };
