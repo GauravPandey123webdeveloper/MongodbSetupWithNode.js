f4f16916d01b3b01a36056a12bbc3680f51e4cca
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-04-08 13:04:56 +0530

    unique user

--- SDMS/src/controllers/studentController.mjs
+++ SDMS/src/controllers/studentController.mjs
@@ -5,17 +5,27 @@ const findStudent = async (req, res) => {
   res.status(200).send({ status: true, message: students });
 };
 const createStudent = async (req, res) => {
-  const data = req.body;
-  const createdStudent = await studentModel.create(data);
-  //    const createdStudent= await studentModel.insertMany([data,data,data]);
-  return res.status(201).send({ status: true, message: createdStudent });
+    try {
+    const data = req.body;
+    const {rollNo,email}=req.body;
+    const checkUser=await studentModel.findOne({email:email,rollNo:rollNo});
+    console.log(checkUser);
+     if(checkUser){
+        return res.status(200).send({status:"failed",message:"user already exits"});
+     }
+    const createdStudent = await studentModel.create(data);
+    //    const createdStudent= await studentModel.insertMany([data,data,data]);
+    return res.status(201).send({ status: true, message: createdStudent });
+    } catch (error) {
+        return res.status(500).send({status:"failed",message:error.message})
+    }
+        
+  
 };
 const deleteStudent = async (req, res) => {
   // const data= await studentModel.deleteMany({name:'tiwari'});
   try {
-    const data = await studentModel.findByIdAndDelete({
-      _id: "660f8e593b4a8a4a0efc47d@",
-    });
+    const data = await studentModel.deleteOne({"email":"abc@gmail.com"});
     return res.status(200).send({ status: true, message: data });
   } catch (error) {
     return res.status(500).send({status:'failed',message:error.message})
